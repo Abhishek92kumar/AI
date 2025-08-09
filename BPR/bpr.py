37ba84cb8fc7fc8fe09ef5ac7fa3950fb620cce6
Abhishek Kumar <abhishek92kumar92@gmail.com>
2025-08-09 12:09:37 +0530

    Update bpr.py

--- BPR/bpr.py
+++ BPR/bpr.py
@@ -38,6 +38,8 @@ def substitute_class(location):
         'KK108-RM15-2025-122783': 'CRO',
         'KK108-MW06-2025-121101': 'CTYG',
         'KK108-RM15-2025-124776': 'CRO',
+        'KK108-RM15-2025-122783': 'CRO',
+        
         
             }
     return class_substitutions.get(location, location)
