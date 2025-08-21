16d5a9782cc0e68ad53ed209904c2b37d39294a7
Abhishek Kumar <abhishek92kumar92@gmail.com>
2025-08-21 09:26:22 +0530

    Update bpr.py

--- BPR/bpr.py
+++ BPR/bpr.py
@@ -131,6 +131,5 @@ st.title("Class Schedule Viewer")
 
 # Fetch the URL from Streamlit secrets
 ics_url = "https://outlook.office365.com/owa/calendar/888f3bb6c2904fd39d8c125e42b7ab8d@aakashicampus.com/bcbe1538d6f34d84b4fe1ab75d7d6d0410158316872069178778/calendar.ics"  # Fetch the URL from Streamlit secrets
-
 if st.button("Fetch and Display Schedule"):
     sort_and_display_last_5_days(ics_url)
