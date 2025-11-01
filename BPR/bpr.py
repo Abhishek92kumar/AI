1cf92374f3289055218dbbfe58436cf05db5604d
Abhishek Kumar <abhishek92kumar92@gmail.com>
2025-11-01 23:23:17 +0530

    Comment out unknown description handling
    Comment out the return statement for unknown description.

--- BPR/bpr.py
+++ BPR/bpr.py
@@ -226,8 +226,8 @@ def get_day(start_time):
     return start_time.strftime("%A")
 
 def get_class_from_description(description):
-    if not description:
-        return "Unknown"
+    # if not description:
+    #     return "Unknown"
     desc = description.upper()
     if "RM15" in desc:
         return "CRO"
